1629144af3c556fd065c8d6f45139829caeca182
Muhammad Kashif <mk.kashif.se@gmail.com>
2023-07-07 03:21:53 +0500

    design fixes done

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
   const [recipes, setRecipes] = useState([]);
   const [filteredIngredients, setFilteredIngredients] = useState([]);
   const [ingredients, setIngredients] = useState([]); // [ {strIngredient1: "Chicken"}, {strIngredient2: "Beef"}  ]
-  const [search, setSearch] = useState('');
+  const [selectedIngredient, setSelectedIngredient] = useState('');
 
 
   const fetchRecipes = async (searchTerm) => {
+    setSelectedIngredient(searchTerm)
     const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`);
     const data = await response.json();
     setRecipes(data.meals);
@@ -33,33 +34,57 @@ function App() {
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
       const data = await response.json();
       setIngredients(data.meals);
+      setFilteredIngredients(data.meals);
     }
 
     fetchIngredients()
   }, [])
 
-
-  return (
-    <main className="bg-yellow-50 min-h-screen flex">
-      <div className='p-4 border-r w-1/5 flex flex-col h-screen space-y-2'>
-        <div>
-          <h2 className='text-2xl font-bold'>Ingredients</h2>
-          <input type='text' placeholder='Search' className='border p-2' onKeyUp={(e) => onKeyUp(e)} />
+  /*
+  <main className="bg-yellow-50 min-h-screen ">
+        <div className='flex justify-between items-center p-4 fixed bg-white w-full'>
+          <h2 className='text-2xl font-bold'>{selectedIngredient}</h2>
+          <div className='text-sm text-gray-600 font-medium'>Total Recipes Found {recipes.length} </div>
         </div>
-        <div className='bg-white h-screen overflow-scroll flex-1 border cursor-pointer'>
+        <div className='flex pt-16'>
+          <div className='p-4 border-r w-1/5 flex flex-col h-screen space-y-2'>
+            <div>
+              <h2 className='font-bold'>Ingredients</h2>
+              <input type='text' placeholder='Search' className='border w-full p-2 rounded-lg' onKeyUp={(e) => onKeyUp(e)} />
+            </div>
+            <div className='bg-white p-2 rounded-lg h-screen overflow-scroll flex-1 border cursor-pointer'>
+              
+            </div>
+          </div>
+          <div className='overflow-scroll h-screen w-4/5'>
+  
+            
+          </div>
+        </div>
+      </main>
+  
+  */
+  return (
+    <div className='flex h-screen py-8'>
+      <div className='w-1/5 px-4 flex flex-col'>
+        <h2 className='text-gray-700 font-bold'>Ingredients</h2>
+        <div>
+          <input type='text' placeholder='Search' className='border w-full p-2 rounded-lg' onKeyUp={(e) => onKeyUp(e)} />
+        </div>
+        <div className='flex-1 overflow-scroll cursor-pointer'>
           {
             filteredIngredients.map((ingredient) => <div onClick={() => fetchRecipes(ingredient.strIngredient)} className='p-2 rounded-lg px-4 hover:bg-blue-100' key={ingredient.idIngredient}>{ingredient.strIngredient}</div>)
           }
         </div>
       </div>
-      <div className='overflow-scroll h-screen'>
-        <div className='grid grid-cols-4 gap-4 p-4 '>
+      <div className='w-4/5 px-4 border-l overflow-scroll'>
+        <div className='grid grid-cols-4 gap-4'>
           {
             recipes.map((recipe) => <RecipeCard key={recipe.idMeal} recipe={recipe} />)
           }
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
